3f61b036a7536106605338aa856ba9fff15a5ae5
Omulosi <mulongojohnpaul@gmail.com>
2018-11-29 13:58:17 +0300

    [Feature #162299069] Create a database model for users and records and implement API endpoint functionality

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -9,7 +9,9 @@
 """
 
 from flask import Blueprint
+from flask_restful import Api
 
 bp = Blueprint('v1', __name__)
+api_bp = Api(bp)
 
 from app.api.v1 import views, models, errors
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -6,69 +6,83 @@
 
 """
 
-from flask_restful import Resource, reqparse, abort, Api
-from . import bp
+from flask_restful import Resource, reqparse, abort, url_for
+from . import api_bp
 from .models import Record
 
-API = Api(bp)
-
 class RedflagListAPI(Resource):
+    """
+    Implements methods for creating a record and returning a collection
+    of records.
+    """
 
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('type', type=str, required=True,
-                help='record type not provided')
-        self.parser.add_argument('comment', type=str, 
-                help='comment not provided', default="")
-        self.parser.add_argument('location', type=str,
-                help='location not provided', default="")
+        self.parser.add_argument('type', type=str, required=True, help='record type not provided')
+        self.parser.add_argument('comment', type=str, required=True, help='comment not provided')
+        self.parser.add_argument('location', type=str, required=True, help='location not provided')
         super(RedflagListAPI, self).__init__()
 
     def get(self):
         """
         Returns a collection of all red-flag records
         """
-        out = {'status': 200,
-                'data': []
-                }
-        return out
+        output = {'status': 200, 'data': Record.all()}
+        return output
 
     def post(self):
         """
         Creates a new red-flag record
         """
-        out = self.parser.parse_args() # Dictionary of input data
-        return out, 201
+        data = self.parser.parse_args() # Dictionary of input data
+        record = Record(_type=data['type'], location=data['location'],
+                        comment=data['comment'])
+        uri = url_for('v1.redflag', _id=record.data_id, _external=True)
+        record.add_field('uri', uri)
+        Record.put(record)
+        output = {'status': 201,
+                  'data': [{"id": record.data_id, "message": "Created a red-flag record"}]
+                 }
+
+        return output, 201, {'Location': uri}
 
 class RedflagAPI(Resource):
+    """
+    Implements methods for manipulating a particular record
+    """
 
     def get(self, _id):
         """
         Returns a single red-flag record
         """
-
-        out = {'status': 200,
-                'data': []
-                }
-        return out
+        record = Record.by_id(_id)
+        if not record:
+            abort(404, message="Record does not exist")
+        output = {'status': 200,
+                  'data': [record.serialize]
+                  }
+        return output
 
     def delete(self, _id):
         """
         Deletes a red-flag record
         """
-        
-        out = {'status': 200,
-                'data': []
-                }
+
+        record = Record.by_id(_id)
+        if not record:
+            abort(404, message="Record does not exist")
+        Record.delete(_id)
+        out = {}
+        out['status'] = 200
+        out['data'] = [{'id':_id, 'message': 'red-flag record deleted'}]
         return out
 
 class RedflagUpdateAPI(Resource):
     """
-    Updates a red-flag record
+    Updates the location or comment field of red-flag record
     """
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('type', type=str)
         self.parser.add_argument('comment', type=str)
         self.parser.add_argument('location', type=str)
         super(RedflagUpdateAPI, self).__init__()
@@ -77,12 +91,27 @@ class RedflagUpdateAPI(Resource):
         """
         Updates a field of a red-flag record
         """
-        out = self.parser.parse_args()
-        return out, 201
+        data = self.parser.parse_args()
+        record = Record.by_id(_id)
+        if not record:
+            abort(404, message="Record does not exist")
+        if field == 'location':
+            new_location = data.get('location')
+            if new_location is not None:
+                record.location = new_location
+        elif field == 'comment':
+            new_comment = data.get('comment')
+            if new_comment is not None:
+                record.comment = new_comment
+        Record.put(record)
+        output = {}
+        output['status'] = 201
+        output['data'] = [{"id": _id, "message": "Updated red-flag record's " + field}]
+        return output, 201
 #
 # API resource routing
 #
 
-API.add_resource(RedflagListAPI, '/red-flags')
-API.add_resource(RedflagAPI, '/red-flags/<int:_id>')
-API.add_resource(RedflagUpdateAPI, '/red-flags/<int:_id>/<field>')
+api_bp.add_resource(RedflagListAPI, '/red-flags', endpoint='redflags')
+api_bp.add_resource(RedflagAPI, '/red-flags/<int:_id>', endpoint='redflag')
+api_bp.add_resource(RedflagUpdateAPI, '/red-flags/<int:_id>/<field>', endpoint='update_redflag')
