a46ae942dcea2e08b71c888e3bbffbe26df31a6e
Omulosi <mulongojohnpaul@gmail.com>
2018-12-02 04:37:30 +0300

    [Bug #162365733] Fix database model

--- app/tests/v1/test_models.py
+++ app/tests/v1/test_models.py
@@ -2,7 +2,8 @@
     app.tests.v1.models
     ~~~~~~~~~~~~~~~~~~~
 
-    Tests for the database model
+    Tests for the database's Record model
+
 """
 
 import unittest
@@ -12,8 +13,9 @@ class RecordModel(unittest.TestCase):
 
     def setUp(self):
         # Create record objects but dont store them in the database
-        self.r1 = Record(location="-1, 36", comment="Judges soliciting for bribes")
-        self.r2 = Record(location="18, 55", comment="Judges soliciting for bribes")
+        self.data = {'location': "-1, 36", 'comment': "Judges soliciting for bribes"}
+        self.r1 = Record(**self.data)
+        self.r2 = Record(**self.data)
 
     def tearDown(self):
         Record.clear_all()
@@ -26,7 +28,7 @@ class RecordModel(unittest.TestCase):
         self.assertEqual(len(Record.all()), 2)
 
     def test_get_by_id(self):
-        r = Record(location="-1, 36", comment="Judges soliciting for bribes")
+        r = Record(**self.data)
         Record.put(r)
         r_id = r.data_id
         id_2 = 'id_two'
@@ -44,7 +46,7 @@ class RecordModel(unittest.TestCase):
                 Record.put([{'id': 5}])
 
     def test_delete(self):
-        r = Record(location="-1, 36", comment="Judges soliciting for bribes")
+        r = Record(**self.data)
         Record.put(r)
         r_id = r.data_id
         self.assertEqual(Record.delete(r_id), True)
@@ -52,13 +54,21 @@ class RecordModel(unittest.TestCase):
         self.assertEqual(Record.delete('key'), None)
 
     def test_serialize(self):
-        r = Record(location="-1, 36", comment="Judges soliciting for bribes")
+        r = Record(**self.data)
         self.assertEqual(type(r.serialize), dict)
+        self.assertEqual(len(r.serialize), 10)
 
     def test_add_field(self):
-        r = Record(location="-1, 36", comment="Judges soliciting for bribes")
+        r = Record(**self.data)
         r.add_field('name','test')
         self.assertEqual(r.name, 'test')
+
+    def test_clear_all(self):
+        r = Record(**self.data)
+        Record.put(r)
+        self.assertEqual(Record.clear_all(), None)
+        self.assertEqual(Record.all(), [])
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
