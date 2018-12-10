03de8ee897d66ac813be1cf2d027685d75ae54e2
Omulosi <mulongojohnpaul@gmail.com>
2018-12-10 04:50:00 +0300

    [Chore #162533800] Add convenience functions for managing the database

---
+++ app/cli.py
@@ -0,0 +1,70 @@
+"""
+    app.cli
+    ~~~~~~~~~~~
+
+    Provides convenience commands for manipulating the database
+
+"""
+
+from app import app
+from db import Model
+import click
+
+@app.cli.group()
+def db():
+	"""
+	Database management commands
+	"""
+	pass
+
+@db.command()
+def create_records():
+	"""
+	Create a new records table if one does not exist
+	"""
+	Model.create_records_table()
+
+
+@db.command()
+def create_users():
+	"""
+	Create a new users table if one does not exist
+	"""
+	Model.create_users_table()
+
+@db.command()
+def clear_all():
+	"""
+	Clear all existing tables. (users and records)
+	"""
+	Model.clear_all_tables()
+
+@db.command()
+@click.argument('name')
+def clear_table(name):
+	"""
+	Clear a specific table given table name
+	"""
+	Model.clear_table(name)
+
+@db.command()
+def drop_all():
+	"""
+	Delete all existing tables. (users and records)
+	"""
+	Model.drop_all_tables()
+
+@db.command()
+@click.argument('name')
+def drop_table(name):
+	"""
+	Delete a specific table given table name
+	"""
+	Model.drop_table(name)
+
+@db.command()
+def close_db():
+	"""
+	Closes the database connection
+	"""
+	Model.close_connection()
