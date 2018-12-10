8e7457d1b480b8e68d9904aa646bb1c652cde804
Omulosi <mulongojohnpaul@gmail.com>
2018-12-10 04:46:30 +0300

    [Chore #162533800] Add v2 api blueprint

---
+++ app/api/v2/__init__.py
@@ -0,0 +1,17 @@
+"""
+    app.api.v2
+    ~~~~~~~~~~~
+
+    Version 2 of the RESTful API
+
+    Creates a Blueprint instance and imports all necessary modules
+
+"""
+
+from flask import Blueprint
+from flask_restful import Api
+
+bp = Blueprint('v2', __name__)
+api_bp = Api(bp)
+
+from app.api.v2 import models
