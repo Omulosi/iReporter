c12f84c941f7329ef3a5e8a4b3d1b7589480735a
Omulosi <mulongojohnpaul@gmail.com>
2019-04-11 12:31:32 +0300

    update schema sql code

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,19 +1,23 @@
-create table if not exists records (
-            id serial primary key,
-            type varchar(50) not null,
-            comment varchar(140) not null,
-            location varchar(30) not null,
-            status varchar(50) not null,
-            createdon timestamp with time zone not null default now(),
-            images varchar(120)[] not null,
-            videos varchar(120)[] not null,
-            uri varchar(140),
-            createdby integer not null,
-            user_id integer references users(id) on delete cascade not null
-            );
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS records;
+DROP TABLE IF EXISTS blacklist;
+
+create table records (
+      id serial primary key,
+      type varchar(50) not null,
+      comment varchar(140) not null,
+      location varchar(30) not null,
+      status varchar(50) not null,
+      createdon timestamp with time zone not null default now(),
+      images varchar(120)[] not null,
+      videos varchar(120)[] not null,
+      uri varchar(140),
+      createdby integer not null,
+      user_id integer references users(id) on delete cascade not null
+);
             
 
-create table if not exists users (
+create table users (
             id serial primary key,
             username varchar(80) not null,
             email varchar(100) not null,
@@ -26,7 +30,7 @@ create table if not exists users (
             password_hash varchar(100) not null
             );
 
-create table if not exists blacklist (
+create table blacklist (
             id serial primary key,
             jti varchar(140) not null
             );
